41a63a61553c220e0727cbc7284d63e1a3d1a56c
Gabriel Richard <87505238+gabrielrichard01@users.noreply.github.com>
2022-03-01 16:57:18 -0300

    função Parte 1

---
+++ modulo8-javascript-basico/aula18(funçoes)/script.js
@@ -0,0 +1,10 @@
+function media(n1, n2){
+    var nota1 = n1;
+    var nota2 = n2;
+    var media = (nota1 + nota2) / 2;
+    // console.log(media);
+    return media;
+}
+
+var resultado1 = media(6, 7);
+var resultado2 = media(8, 7);
