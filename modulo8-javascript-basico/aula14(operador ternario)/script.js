7f7f15585ed82f307036435c584595003d78934f
Gabriel Richard <87505238+gabrielrichard01@users.noreply.github.com>
2022-03-01 13:48:10 -0300

    condicionais if

---
+++ modulo8-javascript-basico/aula14(operador ternario)/script.js
@@ -0,0 +1,10 @@
+var idade = 19;
+
+if (idade < 18 || idade > 70) {
+    console.log("Pode");
+    console.log("Qual é o seu pedido?");
+}
+else {
+    console.log("Não pode");
+    console.log("Volte futuramente");
+}
