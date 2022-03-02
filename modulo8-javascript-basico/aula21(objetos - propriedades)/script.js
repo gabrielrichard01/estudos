d09fbaf2a81745e5c2785475f756e63e3a82e23a
Gabriel Richard <87505238+gabrielrichard01@users.noreply.github.com>
2022-03-02 15:19:38 -0300

    Objetos - propriedades

---
+++ modulo8-javascript-basico/aula21(objetos - propriedades)/script.js
@@ -0,0 +1,32 @@
+// var aluno = {
+//     nome: "Maria",
+//     nota1: [7.5, 5.0],
+// };
+
+// console.log(aluno.nome, aluno.nota1, aluno.nota2, aluno.matricula)
+
+// var aluno = {
+//     nome: "Maria",
+//     nota1: [7.5, 5.0],
+// };
+
+// var aluno2 = {
+//     nome: "João",
+//     nota1: [8.5, 7.0],
+// };
+
+// console.log(aluno);
+// console.log(aluno2);
+
+var aluno = {
+    nome: "Maria",
+    nota1: [7.5, 5.0],
+};
+
+var nomeDeUmaPropriedade = "Sobrenome";
+
+aluno.matricula = "12345";
+
+aluno[nomeDeUmaPropriedade] = "Silva";
+
+console.log(aluno);
