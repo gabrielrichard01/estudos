b18ce60a0f1a5c7b50dc1891453664aaf19cef60
Gabriel Richard <87505238+gabrielrichard01@users.noreply.github.com>
2022-03-05 21:16:02 -0300

    projeto - churrascômetro

---
+++ modulo10-projeto-churrascometro/aula04(estilo css)/script.js
@@ -0,0 +1,5 @@
+//  Carne - 400 gr por pessoa + de 6 horas - 650
+//  Cerveja - 1200ml por pessoa + de 6 horas - 2000
+//  Refrigerante/agua - 1000ml por pessoa + de 6 horas - 1500
+
+//  Criança valem por 0,5
