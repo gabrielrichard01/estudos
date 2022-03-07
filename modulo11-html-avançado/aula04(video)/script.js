99c63ab356febd1e1d13ea403298d9e1ee3c32a3
Gabriel Richard <87505238+gabrielrichard01@users.noreply.github.com>
2022-03-07 13:50:22 -0300

    video

---
+++ modulo11-html-avançado/aula04(video)/script.js
@@ -0,0 +1,16 @@
+let lista = document.getElementById("lista");
+
+lista.dataset.n = 4;
+
+let id = parseInt(lista.getAttribute("data-id"));
+console.log(id);
+
+let num = parseInt(lista.dataset.n);
+
+let conteudo = "";
+
+for (let i = 0; i < num; i++){
+    conteudo += "<li>" + i + "</li>";
+}
+
+lista.innerHTML = conteudo;
